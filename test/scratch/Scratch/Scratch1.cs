2e6d11b91371c2983cc46dddb3751fa09a92c0b3
osterhoutan-UofU <andrew.osterhout@utah.edu>
2020-11-13 13:26:01 -0700

    GEN: Added a scratch project This will allow ut to do basic concept testig with less headache.

---
+++ test/scratch/Scratch/Scratch1.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * << Scratch Code File >>
+ ** This is a file for testing out code on the fly
+ ** It relies of the CodeRunner vs code extension 
+ **   and the scriptcs utility (https://scriptcs.net)
+ **   in order to run without having to build everything.
+ ** Therefore, nothing in this file can be contained 
+ **   in a namespace, if you want to run & debug it, 
+ **   without having to deal w/ MSBuild as vs sol system.
+ * 
+ * Team: JustBroken
+ * Authors: 
+ *   + Andrew Osterhout (u1317172)
+ *   + Brighton Fox (u0981544)
+ * Organization: University of Utah
+ *   Course: CS3500: Software Practice
+ *   Semester: Fall 2020
+ * 
+ * Version Data: 
+ *   + ...
+ */
+
+using System;
+
+
+public class Scratch1
+{
+}
+
